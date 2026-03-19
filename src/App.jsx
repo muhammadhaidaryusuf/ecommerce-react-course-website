b2538ed49277899ebdbde17fa86c1425fa1eaf52
muhammadhaidaryusuf <muhammadhenandra2003@gmail.com>
2026-03-19 12:20:55 +0700

    menyelesaikan fitur cartItem dan ViewDetails

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import AuthProvider from "./context/AuthContext";
+import AuthProvider from "./context/AuthProvider";
+import ProductDetails from "./pages/ProductDetails";
+import CartProvider from "./context/Cart";
 
 function App() {
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </CartProvider>
     </AuthProvider>
   );
 }
